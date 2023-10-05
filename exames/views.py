1bb0d2fe36a457fc7e036f4a9f7c1af6af5c394e
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-10-05 18:10:53 -0300

    VitaLab

--- exames/views.py
+++ exames/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+
+
 def solicitar_exames(request):
-    if not (request.user.is_authenticated)
-        return  HttpResponse
+    if not (request.user.is_authenticated):
+        return  HttpResponse("Você não pode acessar aqui")
     return HttpResponse('Estou aqui')
 
