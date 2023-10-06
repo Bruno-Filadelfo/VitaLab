7c7a36fd66be807e4ff14561bbeaca715f5cd68f
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-10-06 17:30:54 -0300

    VitaLab

--- exames/views.py
+++ exames/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from .models import TiposExames
 @login_required
 def solicitar_exames(request):
     if request.method == "GET":
-       return render(request, 'solicitar_exames.html')
+       tipos_exames = TiposExames.objects.all()
+       for i in tipos_exames:
+           print(i.nome)
+           print(i.preco)
+           print('- * 10')
+       return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
 
