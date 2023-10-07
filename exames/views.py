c0630f43319318eeda9db60d5b80def33b6c79ab
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-10-07 20:08:50 -0300

    VilaLab

--- exames/views.py
+++ exames/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames
+from .models import TiposExames, PedidosExames
+from datetime import datetime
+
 
 @login_required
 def solicitar_exames(request):
@@ -20,3 +22,13 @@ def solicitar_exames(request):
                preco_total += i.preco
             
         return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames, 'solicitacao_exames': solicitacao_exames, 'preco_total': preco_total})
+    
+def fechar_pedido(request):
+    exames_id = request.POST.getlist('exames')
+    pedido_exame = PedidosExames(
+        usuario=request.user,
+        date=datetime.now()
+    )
+    pedido_exame.save()
+    print(exames_id)
+    return HttpResponse('estou aqui')    
