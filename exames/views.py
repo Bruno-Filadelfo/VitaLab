17e6e783426c95e3ef64662762c0082088425553
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-10-07 04:45:22 -0300

    VitaLab

--- exames/views.py
+++ exames/views.py
@@ -9,6 +9,11 @@ def solicitar_exames(request):
        tipos_exames = TiposExames.objects.all()
        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
     elif request.method == "POST":
-        exames_id = request.POST.get('exames')
-        print(exames_id)
-        return HttpResponse('teste')
+        exames_id = request.POST.getlist('exames')
+        solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+        # calcular preço dos dados disponíveis 
+        preco_total = 0
+        for i in solicitacao_exames:
+            preco_total += i.preco
+            
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
