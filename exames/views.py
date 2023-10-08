5ee957dfbbcb5b5b2850fc4178091448f73001cb
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-10-08 02:32:12 -0300

    VitaLab

--- exames/views.py
+++ exames/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames, PedidosExames
+from .models import TiposExames, PedidosExames, SolicitacaoExame
 from datetime import datetime
 
 
@@ -9,26 +9,38 @@ from datetime import datetime
 def solicitar_exames(request):
     tipos_exames = TiposExames.objects.all()
     if request.method == "GET":
-       return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
+        return render(request, "solicitar_exames.html", {"tipos_exames": tipos_exames})
     elif request.method == "POST":
-        exames_id = request.POST.getlist('exames')
+        exames_id = request.POST.getlist("exames")
         solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
 
-        # calcular preço dos dados disponíveis 
-        
+        # calcular preço dos dados disponíveis
+
         preco_total = 0
         for i in solicitacao_exames:
             if i.disponivel:
-               preco_total += i.preco
-            
-        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames, 'solicitacao_exames': solicitacao_exames, 'preco_total': preco_total})
-    
+                preco_total += i.preco
+
+        return render(
+            request,
+            "solicitar_exames.html",
+            {
+                "tipos_exames": tipos_exames,
+                "solicitacao_exames": solicitacao_exames,
+                "preco_total": preco_total,
+            },
+        )
+
+
 def fechar_pedido(request):
-    exames_id = request.POST.getlist('exames')
-    pedido_exame = PedidosExames(
-        usuario=request.user,
-        date=datetime.now()
-    )
+    exames_id = request.POST.getlist("exames")
+    solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+    pedido_exame = PedidosExames(usuario=request.user, data=datetime.now())
     pedido_exame.save()
-    print(exames_id)
-    return HttpResponse('estou aqui')    
+
+    for exame in solicitacao_exames:
+        solicitacao_exames_temp = SolicitacaoExame(
+            usuario=request.user, exame=exame, status="E"
+        )
+        solicitacao_exames_temp.save()
+    return HttpResponse("estou aqui")
