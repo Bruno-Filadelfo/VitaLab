e501af4225e1caba20928f4ee5a18f1880fcd66a
Bruno Filadelfo <bruno.filadelfo@outlook.com>
2023-11-06 17:58:09 -0300

    Portfólio em andamento

--- portfólio/views.py
+++ portfólio/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db.models.functions import Concat
+from django.db.models import Value
 
 def gerenciar_clientes(request):
     clientes = User.objects.filter(is_staff=False)
+    
+    nome_completo = request.GET.get('nome')
+    email = request.GET.get('email')
 
+    if email:
+        clientes = clientes.filter(email__contains = email)
+
+    if nome_completo:
+        clientes = clientes.annotate(full_name=Concat('first_name', Value(' '), 'last_name')).filter(full_name__contains=nome_completo)
+
+
+    print(f'{nome_completo} - {email}')
     return render(request, 'gerenciar_clientes.html', {'clientes': clientes})
